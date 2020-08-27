5fd6a955c59749ea4a1f143cbf160eaaf6201131
sayerhs <shreyas@umd.edu>
2020-08-27 12:48:44 +0000

    Deploying to gh-pages from  @ 4edb954a06bba0c70c6c3d3ec96c1523332dc543 🚀

--- search/all_7.js
+++ search/all_7.js
@@ -5,6 +5,7 @@ var searchData=
   ['has_5fdiffusion',['has_diffusion',['../structamr__wind_1_1pde_1_1_density.html#ad4dbe0df40d452b4042dfd89c6a87c85',1,'amr_wind::pde::Density::has_diffusion()'],['../structamr__wind_1_1pde_1_1_i_c_n_s.html#add5811d5e2e54536256b573675e8c678',1,'amr_wind::pde::ICNS::has_diffusion()'],['../structamr__wind_1_1pde_1_1_vector_transport.html#a6d6a088cea260c460fc0ada76dd6d328',1,'amr_wind::pde::VectorTransport::has_diffusion()'],['../structamr__wind_1_1pde_1_1_scalar_transport.html#a8e38b4f026d14415280dd73f3c6f210e',1,'amr_wind::pde::ScalarTransport::has_diffusion()']]],
   ['has_5fdim',['has_dim',['../classncutils_1_1_n_c_group.html#ac233f0c14bd6b340fcd5a14da53b5042',1,'ncutils::NCGroup']]],
   ['has_5fextdir',['has_extdir',['../namespaceamr__wind_1_1utils.html#ab34425b6e44fee3d4921b0781c1965c0',1,'amr_wind::utils']]],
+  ['has_5fextdir_5for_5fho',['has_extdir_or_ho',['../namespaceamr__wind_1_1utils.html#a8d199132950cc64edbd00ad817000dd2',1,'amr_wind::utils']]],
   ['has_5ffillpatch_5fop',['has_fillpatch_op',['../classamr__wind_1_1_field.html#afdc4ec936ae7baa4619c0c1f6de0e026',1,'amr_wind::Field']]],
   ['has_5fgroup',['has_group',['../classncutils_1_1_n_c_group.html#a00ea1ccd06adafc43396239ec842b23f',1,'ncutils::NCGroup']]],
   ['has_5foverset',['has_overset',['../classamr__wind_1_1_c_f_d_sim.html#ad413351ee8d8f9a1a6426381b901722d',1,'amr_wind::CFDSim']]],
